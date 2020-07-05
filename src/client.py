aff4757d4a5968f7eb7344f2c956bd1cf3e8f2c6
Viktor Melker <viktormelker@gmail.com>
2020-07-05 22:38:20 +0200

    Cleanup

--- src/client.py
+++ src/client.py
@@ -10,12 +10,17 @@ base_url = 'https://tdb.ridsport.se'
 def login():
     params = {
         'utf8': '✓',
-        # 'authenticity_token': ''
+        'authenticity_token': 'OIXIaid7lMuIAUSNyCUClcB9vEZfraPN1Hv726VjTC2cMIyPaV8UhAKpXRzJUbhWWK49zsW6fpHSRpbunBGYfw==',
         'email': username,
-        # 'return_to': ''
+        'return_to': '',
         'password': password,
         'commit': 'Logga+in',
     }
     url = base_url + '/session'
     response = requests.post(url, data=params)
-    breakpoint()
+    write_file('login_response.html', response.text)
+
+
+def write_file(filename: str, text: str):
+    with open(filename, 'w') as html_file:
+        html_file.write(text)
